b9f32fa346184666a496df74d9cbef1ef0ab1719
Ujjwal Kuikel <ujjwal.kuikel@springfive.com>
2022-09-13 11:33:19 +0530

    def

--- src/MyComponent/Header.js
+++ src/MyComponent/Header.js
@@ -18,11 +18,6 @@ export default function Header(props) {
         </li>
         
       </ul>
-
-      {props.searchBar ? <form className="form-inline my-2 my-lg-0">
-        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>: "No bar"}
     </div>
   </nav>
   )
