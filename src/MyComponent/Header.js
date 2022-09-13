8b0c020d943388790f3a858e3a040d0711787f5c
Ujjwal Kuikel <ujjwal.kuikel@springfive.com>
2022-09-13 11:48:39 +0530

    header href fixed

--- src/MyComponent/Header.js
+++ src/MyComponent/Header.js
@@ -3,22 +3,7 @@ import React from 'react'
 export default function Header(props) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand">{props.title}</a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-  
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item active">
-          <a className="nav-link">Home <span className="sr-only">(current)</span></a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link">Link</a>
-        </li>
-        
-      </ul>
-    </div>
+    <a className="navbar-brand" href='https://ujjwalkuikel.github.io/ToDoApp/'>{props.title}</a>
   </nav>
   )
 }
